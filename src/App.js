93c04e31c0c0821eef509e9c60f310d2d48c2b72
github MinamisoutH <kanou373@gmail.com>
2021-06-02 22:43:42 +0900

    Station4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 // DO NOT DELETE
 
 import * as React from 'react'
+import { useState } from 'react'
 import './App.css'
 
 /**
  *
  * @type {React.FC}
  */
+
 export const App = () => {
+  //dogUrlの初期値
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/frise-bichon/3.jpg")
   return (
     <header>
       <h1>Dog App</h1>
       <p>ワンちゃん画像を集めたサイト</p>
-      <img src="https://images.dog.ceo/breeds/frise-bichon/3.jpg"/>
+      <img src={dogUrl}/>
     </header>
   )
 }
