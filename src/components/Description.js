0bc0529f675d42cf680740d5251d5d0008b735eb
github MinamisoutH <kanou373@gmail.com>
2021-06-06 00:41:33 +0900

    Station8

---
+++ src/components/Description.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState } from 'react'
+import DogImage from './DogImage.js'
+
+const Description = (props) => {
+    const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/frise-bichon/3.jpg")
+    return (
+        <div className="content">
+        <div className="description">
+          <p>{props.description}</p>
+        </div>
+        <div className="image">
+          <DogImage url={dogUrl}/>
+          <button className="btn" onClick={() => fetch("https://dog.ceo/api/breeds/image/random")
+            .then(res => {
+              return res.json();
+            })
+            .then(myJson => {
+              setDogUrl(myJson['message'])
+            })
+          }>{props.text}</button>
+        </div>
+      </div>
+    )
+};
+
+export default Description
+
---
+++ src/components/DogImage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const DogImage = (props) => {
+    return  (
+        <div>
+            <img
+                src={props.url}
+                className="dog-img"
+                alt="ワンちゃん画像"
+            />
+            <br/>
+        </div>
+    )
+};
+
+export default DogImage
