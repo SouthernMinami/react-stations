a37a107334c06d8d0e47ea2aa9de0110d8e859e5
github MinamisoutH <kanou373@gmail.com>
2021-06-06 00:35:23 +0900

    Station8

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 // DO NOT DELETE
 
 import * as React from 'react'
-import { useState } from 'react'
 import './App.css'
+import Header      from './components/Header'
+import Description from './components/Description'
 
 /**
  *
@@ -10,25 +11,16 @@ import './App.css'
  */
 
 export const App = () => {
-  //dogUrlの初期値
-  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/frise-bichon/3.jpg")
   return (
-    <body>
-      <header>
-        <h1>Dog App</h1>
-      </header>
-      <div>
-        <p>ワンちゃん画像を集めたサイト</p>
-        <img src={dogUrl}/>
-        <button onClick={() => fetch("https://dog.ceo/api/breeds/image/random")
-          .then(res => {
-            return res.json();
-          })
-          .then(myJson => {
-            setDogUrl(myJson['message'])
-          })
-        }>更新</button>
-      </div>
-    </body>
+    <div className="App">
+      <Header
+        title={'Dog App'}
+      />
+      <Description
+        description = {'ワンちゃんの画像を集めたサイトです'}
+        text={'更新'}
+      />
+      <hr/>
+    </div>
   )
-}
+};
